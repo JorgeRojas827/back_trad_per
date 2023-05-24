e4d65e77202d88d198672f32e2782c1a3ee1fe8f
jorger-delfosti <jorge.rojas@delfosti.com>
2023-05-23 20:41:48 -0500

    Added categories and restaurants

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      header: '*',
+      origin: ['http://localhost:1337']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
---
+++ config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 465),
+        auth: {
+          user: env("GMAIL_USER"),
+          pass: env("GMAIL_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "noreply@recipee.com",
+        defaultReplyTo: "noreply@recipee.com",
+      },
+    },
+  },
+  "vercel-deploy": {
+    enabled: true,
+  },
+});
